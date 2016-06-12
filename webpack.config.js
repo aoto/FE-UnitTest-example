3c2050a6a66fa2310d8cbafadf583f9553124fb1
shengjie.yuysj <shengjie.yuysj@alibaba-inc.com>
2016-06-12 12:05:33 +0800

    Add es6 browser show.

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
-    './src/browser/index'
+    'mocha!babel!./test/browser/index.spec.js'
   ],
   output: {
     path: path.join(__dirname, 'build'), // 文件存放地址
